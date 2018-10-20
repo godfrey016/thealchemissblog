32744c458fc62ac34a9fc992e333c111c7424570
Godfrey Rails Development <agbrogodfrey@gmail.com>
2018-10-20 06:09:28 -0500

    create template

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+
+	validates :title, presence: true, length: {minimum:10,maximum:20}
+	validates :description, presence: true, length: {minimum:30,maximum:50}
 end
